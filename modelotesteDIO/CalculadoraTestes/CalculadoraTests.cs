69f72b2117566894d271873f527ec03cf90293d0
Ramon_Trigo <ramtrigo@gmail.com>
2024-01-13 22:20:57 -0300

    implementacao teste unitário

---
+++ modelotesteDIO/CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,23 @@
+using Calculadora.Services;
+namespace CalculadoraTestes;
+
+public class CalculadoraTests
+{
+    private CalculadoraImp _calc;
+
+    public CalculadoraTests()
+    {
+          _calc = new CalculadoraImp();
+    }
+    [Fact]
+    public void DeveSomar5Com10Retornar15()
+    {
+          //Arrange
+          int num1 = 5;
+          int num2 =10;
+          //Act
+          int resultado = _calc.Somar(num1,num2);
+          //Assert
+          Assert.Equal(15,resultado);
+    }
+}
